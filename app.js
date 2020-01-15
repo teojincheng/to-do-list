08bc7bae6548ddf33d3a891432ca7ffad2172811
Teo Jin Cheng <jinchengproject@gmail.com>
2020-01-15 11:28:43 +0800

    modify ul to not have first element. modify app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/**
+ * Create li element and set its value equal to value retrieve from input box.
+ * append it to the ul elemnt.
+ */
 function createTodoListItem() {
   const text = "Buy shampoo";
   const li = document.createElement("li");
@@ -5,6 +9,7 @@ function createTodoListItem() {
   li.textContent = input.value;
   const ul = document.querySelector("ul");
   ul.appendChild(li);
+  input.value = "";
 }
 
 const addButton = document.querySelector("button");
